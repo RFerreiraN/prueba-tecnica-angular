8a53fbb54345a34e83891f042cf43234a3b227f1
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-12-17 02:06:52 +0100

    guardamos datos en el local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuarios } from '../models/usuarios.model';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  public usuariosList: Usuarios[] = [];
+  public localStorageKey = 'usuarios'
+  public nombresUsuarios : string[] = []
+
+  constructor(
+    public _usuariosServices: UsuariosService
+  ) { }
+
+  getUsuarios() {
+    this._usuariosServices.getAllUsuarios().subscribe((data: Usuarios[]) => {
+      this.usuariosList = data;
+
+      this.usuariosList.forEach(usuario => {
+        this.nombresUsuarios.push(usuario.name)
+      })
+
+      localStorage.setItem(this.localStorageKey, JSON.stringify(this.nombresUsuarios))
+    })
+  }
+
+
 
 }
